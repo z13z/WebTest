0bd190c4058917b80371b9db6184a6e0bcca9c36
z13z <zgogi15@freeuni.edu.ge>
2017-06-07 22:45:08 +0400

    first commit

---
+++ src/Classes/MyDBInfo.java
@@ -0,0 +1,12 @@
+package Classes;
+
+/**
+ * Created by z13 on 6/7/17.
+ */
+public interface MyDBInfo {
+    public static final String MYSQL_USERNAME = "root";
+    public static final String MYSQL_PASSWORD = "jumbera";
+    public static final String MYSQL_DATABASE_SERVER = "localhost";
+    public static final String MYSQL_DATABASE_NAME = "test_db";
+}
+
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: z13
+  Date: 6/7/17
+  Time: 10:11 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
